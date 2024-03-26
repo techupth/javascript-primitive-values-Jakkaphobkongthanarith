f8020c0d9b8a9d664f8e0a3e919ed9652dfff44e
Jakkaphobkongthanarith <Jakkaphob.k@ku.th>
2024-03-26 19:37:57 +0700

    submit as main, assinmennt 1-8

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,7 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+
+let allItemPrize = 27000;
+let discount = "25";
+let paidPrize = allItemPrize - (allItemPrize * (Number(discount)/100)) ; 
